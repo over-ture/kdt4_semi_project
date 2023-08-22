f85accb5ae027dd6400fbec49c61be725b130b9f
YongJae Kim <golddragon939@gmail.com>
2023-08-22 13:37:36 +0900

    파일 업데이트

--- codereview/accounts/forms.py
+++ codereview/accounts/forms.py
@@ -1,14 +1,13 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CodeReview
-from django.contrib.auth.password_validation import CommonPasswordValidator
-from django.core.exceptions import ValidationError
 
 class CodeReviewForm(forms.ModelForm):
+    title = forms.CharField(max_length=100)
     code = forms.CharField(required=False, widget=forms.Textarea(attrs={'rows': 10, 'cols': 60}))
     class Meta:
         model = CodeReview
-        fields = ['code', 'uploaded_file']
+        fields = ['title', 'code', 'uploaded_file']
 
     def clean(self):
         cleaned_data = super().clean()
@@ -23,24 +22,3 @@ class SignupForm(UserCreationForm):
     username = forms.CharField(required=True, label='아이디', widget=forms.TextInput(attrs={'class': 'form-control'}))
     password1 = forms.CharField(required=True, label='비밀번호', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     password2 = forms.CharField(required=True, label='비밀번호 확인', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-
-    def clean_password1(self):
-        password1 = self.cleaned_data.get('password1')
-
-        # 비밀번호 길이가 8자 이상인지 확인
-        if len(password1) < 8:
-            raise forms.ValidationError("비밀번호는 8자 이상이어야 합니다.")
-
-        common_password_validator = CommonPasswordValidator()
-
-        # 너무 흔한 비밀번호인지 검사하고 오류 메시지 리스트에 추가
-        password_validation_errors = []
-        try:
-            common_password_validator.validate(password1)
-        except ValidationError as error:
-            password_validation_errors.extend(error.messages)
-
-        if password_validation_errors:
-            raise forms.ValidationError("이 비밀번호는 너무 흔한 비밀번호입니다. 다른 비밀번호를 선택하세요.")
-
-        return password1
