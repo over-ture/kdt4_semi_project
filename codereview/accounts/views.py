ac8fb191f8a5d8f40bfe59161b51cf53c094479d
Hyomin Lim <gyals7056@gmail.com>
2023-08-23 10:37:34 +0900

    키 제거

--- codereview/accounts/views.py
+++ codereview/accounts/views.py
@@ -95,7 +95,7 @@ def upload_code(request):
     return render(request, 'accounts/upload_code.html', {'form': form})
 
 
-openai.api_key = "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"
+openai.api_key = "YOUR API KEY"
 
 def get_code_review(input_code):
     response = openai.ChatCompletion.create(
